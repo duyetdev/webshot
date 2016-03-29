e8957a0104f92ebe08870c7667dacd14b88ca90f
Van-Duyet Le <lvduit08@gmail.com>
2016-03-30 00:21:18 +0700

    Update

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http');
+const path = require('path');
 const app = require('koa')();
 
 const middlewares = require('koa-middlewares');
+var serve = require('koa-static-folder');
 const crypto = require('crypto');
-
+var webshot = require('webshot');
 var router = require('koa-router')();
 
 router.all('/', function *(next) {
@@ -12,15 +14,31 @@ router.all('/', function *(next) {
 	var url = this.query.url || '';
 	var is_json = !!this.query.json;
 
+	// URL not found or missing
 	if (!url.length) return this.body = '[url] param is required. Ex: '+ this.request.origin +'/?url=http://duyetdev.com';
 
+	// URL
 	result.url = url;
+	
+	// Hash
 	var hmac = crypto.createHmac('md5', 'a secret');
 	result.hash = hmac.update(url).digest("hex");
 
+	// Created time 
+	result.time = new Date();
+
+	// Screenshot path
+	var shot_path = __dirname + '/shot/'+ result.hash +'.png';
+	result.webshot = this.request.origin + '/shot/' + result.hash +'.png';
+
+	webshot(url, shot_path, function(err) {
+		// screenshot now saved
+	});
+
 	if (is_json) return this.body = result;
 	
-	this.body = result.hash;
+	// Redirect to screenshot
+	this.redirect(result.webshot);
 });
 
 app
@@ -40,6 +58,9 @@ app.use(middlewares.bodyParser({
 app.use(middlewares.session({
 	store: middlewares.RedisStore()
 }));
+
+app.context.assetspath = path.join(__dirname, 'shot');
+app.use(serve('./shot'));
 
 // Start application
 app = module.exports = http.createServer(app.callback());
